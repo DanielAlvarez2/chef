5dc99c1c1dac0caacd269a4d354480993afa24b8
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-03-08 08:28:31 -0500

    create express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+let PORT = "process.env.PORT || 4000"
+
+app.get('/', (req,res)=>{
+    res.send(`Express Server Listening on Port ${PORT} `)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Express Server Listening on Port ${PORT}`);
+})
